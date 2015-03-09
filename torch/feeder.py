487a2bce69f06d969d469b4ffcf5aadff3839536
Shariq Hashme <shariqhashme@gmail.com>
2015-03-09 00:51:46 +0000

    added newline at end

--- torch/feeder.py
+++ torch/feeder.py
@@ -3,5 +3,5 @@ OUT_OF = 4
 I_AM_THE = 0
 # 0 indexed!
 
-mine = sorted(os.listdir('test'))[I_AM_THE::OUT_OF] + ['end']
+mine = sorted(os.listdir('test'))[I_AM_THE::OUT_OF] + ['end\n']
 open('filelist.txt','w').write('\n'.join(mine))
