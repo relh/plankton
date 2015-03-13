174eac3e94f9b4ee4b90b6c885670f94a61d19e3
Shariq Hashme <shariqhashme@gmail.com>
2015-03-13 22:23:51 +0000

    updated classifier

--- torch/classifier.sh
+++ torch/classifier.sh
@@ -1,15 +1,12 @@
-source ~/.bashrc
-cd plankton
-git pull
+# source ~/.bashrc
+# cd plankton/torch
+# git pull
+# ./classifier.sh
 
-cd torch
 cp feeder.py ../..
 cd ../..
-wget http://54.152.122.132:8000/model_4.t7
-rm model_3.t7
-mv model_4.t7 model_3.t7
+wget http://54.173.20.85:8000/model_3.t7
 nano feeder.py
 
 python feeder.py
-cat filelist.txt|th crack.lua>output.txt
-
+nohup cat filelist.txt|th crack.lua>output.txt &
